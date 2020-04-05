8253635424485ff53e4f05e80ed6e5d04fa739a5
makardashe <62598117+makardashe@users.noreply.github.com>
2020-04-05 23:19:51 +0300

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -4,21 +4,53 @@
 
 patients <- read.csv2('https://firebasestorage.googleapis.com/v0/b/uni-sofia.appspot.com/o/data%2FCOVID19_2020_open_line_list%20-%20outside_Hubei_subset.csv?alt=media&token=919a181b-6417-45fd-9314-91497bb4b0fc', stringsAsFactors = FALSE)
 
-## b)
+# b) Number of patients - We look at the number of observations
+str(patients) 
+#or
+nrow(patients)
 
-## c)
+# c) Earliest and latest date of infection confirmation?
+as.Date(patients$date_confirmation) 
+summary(patients$date_confirmation) 
 
-## d)
+# d) What is the average age of the patients?
+mean(patients$age)
 
-## e)
+# e) How many men and how many women are there in the sample? 
+sum(patients$sex == "female")
+sum(patients$sex != "female")
 
-## f)
+#f) Plot the frequency distribution of sex using a bar-chart 
+windows()
+freqGender <- table(patients$sex)
+barplot(freqGender)
+title('Gender distribution')
 
-## g)
+#g) What was the age of the youngest woman?
+isFemale <- patients$sex == "female"
+View (isFemale)
+ageFemale <- patients$age[isFemale]
+min(ageFemale)
 
-## h)
+#h) What was the age of the oldest man?
+isMale <- patients$sex == "male"
+View (isMale)
+ageMale <- patients$age[isMale]
+max(ageMale) 
 
-## i)
+#i) Is there a difference between the average age of male and female patients?
+isMale <- patients$sex == "male"
+View (isMale)
+ageMale <- patients$age[isMale]
+mean(ageMale)
 
-## j)
+isFemale <- patients$sex == "female"
+View (isFemale)
+ageFemale <- patients$age[isFemale]
+mean(ageFemale)
+
+#j) Compare the distribution of age between the countries using a box-plot.
+windows()
+boxplot(age ~ country, data = patients, horizontal = TRUE)
+title('Boxplot for Age by Country')
 
